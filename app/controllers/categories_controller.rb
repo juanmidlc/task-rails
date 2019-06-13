b64c9454501287456ba21b3d76e2456636b86db1
Juan Miguel <juanmidlc@gmail.com>
2019-06-13 14:32:58 -0600

    Controller categories created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,13 +5,34 @@ class CategoriesController < ApplicationController
         render status: 200, json: categories
     end
 
+    def show
+        category = Category.find(params[:id])
+        render json: category
+    end
+
     def create
-        category = new Category(category_params)
+        category = Category.new(category_params)
         if category.save
             render status: 201, json: category
         else
             render status: 422, json: { errors: category.errors }
         end
+    end
+
+    def update
+        category = Category.find(params[:id])
+
+        if category.update(category_params)
+            render status: 200, json: category
+        else
+            render status: 422, json: { errors: category.errors }
+        end
+    end
+
+    def destroy
+        category = Category.find(params[:id])
+        category.destroy
+        head 204
     end
 
     protected
